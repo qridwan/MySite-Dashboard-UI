74ef3d39c15f15b4d8370213b87f98479c56a281
Ridwan <myworld.shoyab@gmail.com>
2021-05-19 00:04:57 +0600

    navigation bar completed

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,37 +4,41 @@ import brandName from "../../assets/img/icons/mYSITE.png";
 import Notification from "../../assets/img/icons/notification.png";
 import Query from "../../assets/img/icons/help.png";
 import styled from "styled-components";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 const Navigation = () => {
   return (
     <Nav>
       <ul>
-       <Brand>
-       <List>
-          <Anchor className="nav-brand" href="#home">
-            <Image src={navIcon} alt="icon" />
-          </Anchor>
-        </List>
-        <BrandName>
-          <Anchor href="#home">
-            <Image src={brandName} alt="brand name" />
-          </Anchor >
-        </BrandName>
-       </Brand>
+        <Brand>
+          <NavIcon>
+            <Anchor href="#home">
+              <Image src={navIcon} alt="icon" />
+            </Anchor>
+          </NavIcon>
+          <BrandName>
+            <Anchor href="#home">
+              <Image src={brandName} alt="brand name" />
+            </Anchor>
+          </BrandName>
+        </Brand>
         <NavLink>
-        <List>
-          <Anchor href="#news">News</Anchor>
-        </List>
-        <List>
-          <Anchor href="#contact">
-          <Image src={Notification} alt="brand name" />
-          </Anchor>
-        </List>
-        <List>
-          <Anchor class="active" href="#about">
-          <Image src={Query} alt="brand name" />
-          </Anchor>
-        </List>
+          <User>
+            <Icon>
+              <AccountCircleIcon />
+            </Icon>
+            <Text>Ridwan</Text>
+          </User>
+          <Notifications>
+            <Anchor href="#notification">
+              <Image src={Notification} alt="brand name" />
+            </Anchor>
+          </Notifications>
+          <Notifications>
+            <Anchor href="#help">
+              <Image src={Query} alt="brand name" />
+            </Anchor>
+          </Notifications>
         </NavLink>
       </ul>
     </Nav>
@@ -43,29 +47,46 @@ const Navigation = () => {
 
 export default Navigation;
 
+const Nav = styled.div``;
 const Brand = styled.div`
-float: left;
-display: flex;
-`
+  float: left;
+  display: flex;
+`;
 const NavLink = styled.div`
-float: Right;
-display: flex;
-`
+  float: Right;
+  display: flex;
+`;
 const Anchor = styled.a`
-text-decoration: none;
-`
-
-const Nav = styled.div`
-
-`
+  text-decoration: none;
+`;
 const BrandName = styled.a`
-margin-top: 10px;
-`
-const List = styled.li`
- margin-right: 15px;
- list-style: none;
+  margin-top: 10px;
+`;
+const NavIcon = styled.li`
+  margin-right: 15px;
+  list-style: none;
+`;
+const User = styled.li`
+  margin-right: 45px;
+  list-style: none;
+  height: 36px;
+  display: flex;
+  align-items: center;
+`;
+const Text = styled.span`
+  font-size: 16px;
+  color: black;
+`;
+const Icon = styled.span`
+  font-size: 32px;
+  margin-right: 5px;
+  color: grey;
+`;
 
-`
-const Image = styled.img`
-
-`
+const Notifications = styled.li`
+  margin-right: 25px;
+  list-style: none;
+  height: 36px;
+  width: 36px;
+`;
+const Image = styled.img``;
